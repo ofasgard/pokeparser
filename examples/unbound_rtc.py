5be162eab553232b5f652f476f4531d64956d570
me <me@springspeech.springspeech.local>
2025-02-09 14:34:18 +0000

    add unbound rtc example

---
+++ examples/unbound_rtc.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+In Pokemon Unbound, an NPC in Frozen Heights will reset your real-time clock if it gets desynced. However, they will only ever do this once per save. This script patches your save file so that they'll reset the RTC for you again.
+"""
+
+import sys
+
+sys.path.append('../')
+import pokeparser
+
+save = pokeparser.SaveGame(sys.argv[1])
+current = save.get_current_save()
+
+# The relevant flag is stored in the "rival info" save game block.
+section = current.get_section_by_id(4)
+buf = bytearray(section.data)
+buf[0xe89] = 0
+section.data = buf
+
+# No need to update the checksum, because unbound doesn't actually check this byte for validation.
+
+save.write("patched.sav")
